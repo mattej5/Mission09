dc6f9da6d0ad6739d12c051412e7d06cb8a9a9c5
mattej5 <matthew.jones6288@gmail.com>
2025-03-03 13:31:20 -0700

    Update title, get rid of extraneous code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Heading from './components/Heading'
 import TeamList from './components/TeamList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <Heading />
         <TeamList />
-      </div>
-      <h1>Hello, world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hello, World!
-        </p>
       </div>
     </>
   )
--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,7 +1,7 @@
 const Heading = () => {
     return (
       <header>
-        <h1>College Basketball Teams Directory</h1>
+        <h1>March Madness Teams Directory</h1>
         <p>Explore teams from all over the country to discover which school they come from, their mascot's name, and their location.</p>
       </header>
     );
